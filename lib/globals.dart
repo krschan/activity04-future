ad6d418a51362fc31eb31deb7863b1630795ea95
krschan <krischxn@gmail.com>
2024-11-06 18:36:57 +0100

    feat(globals): Add colours to read better the console

---
+++ lib/globals.dart
@@ -0,0 +1,8 @@
+// Códigos ANSI para poner colores al texto
+const String reset = '\x1B[0m';
+const String red = '\x1B[31m';
+const String green = '\x1B[32m';
+const String yellow = '\x1B[33m';
+const String blue = '\x1B[34m';
+const String magenta = '\x1B[35m';
+const String cyan = '\x1B[36m';
